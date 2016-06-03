6d34d5177ed66e800f5ee6e73b012356455a7b65
vishalShinde <vcoolvish@yahoo.com>
2016-06-03 11:19:57 +0000

    add regex part in server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 var data = require('./data.js');
 var request = require('request');
 
-// var word = 'pe';
-// var pattern = new RegExp('\\b'+word+'\\b','gi');
+var word = 'pe';
+var pattern = new RegExp('\\b'+word+'\\b','gi');
 
-//   console.log(pattern);
+  console.log(pattern);
   
-//   console.log('-----------------------------------------------------------');
+  console.log('-----------------------------------------------------------');
 
-// for(var i=0; i<data.length; i++){
+for(var i=0; i<data.length; i++){
   
-//   console.log(data[i]);
+  console.log(data[i]);
   
-//   var str = pattern.exec(data[i]);
+  var str = pattern.exec(data[i]);
   
-//   console.log(str);
-// }
+  console.log(str);
+}
 
 var comparingCurrencies = 'inr';
 request('http://apilayer.net/api/historical'+
